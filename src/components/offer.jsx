eea9c0be4151eb1064da29b26ed722e849e2546a
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2023-03-03 14:30:53 +0000

    day 2 end

---
+++ src/components/offer.jsx
@@ -0,0 +1,18 @@
+import '../offer.css'
+const app = () =>{
+    <>
+    <p>dicounts and offers</p>
+    <div className="container">
+    <div className='banner'>
+         discount content
+    </div>
+    <div className='banner'>
+         discount content
+    </div>
+    <div className='banner'>
+         discount content
+    </div>
+    </div>
+    </>
+}
+export default app;
